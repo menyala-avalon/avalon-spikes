21add66d96cca73a326c511ad84c38e93c9a7627
Joel Lim <joel.lim@Joels-MacBook-Pro.local>
2022-05-27 13:30:08 +0800

    add

---
+++ gen.py
@@ -0,0 +1,22 @@
+import os
+
+f = open('lines.txt')
+
+lines = f.readlines()
+
+for line in lines:
+    line = line.strip().strip("\"").strip()
+    url = line
+    filename = line.split("/")[-1]
+    fontname = filename.split(".")[0]
+    print(filename, fontname)
+    outputfile_name = "{}.css".format(fontname)
+    template = """
+@font-face {{
+    font-family: "{}";
+    src: url("{}");
+}}
+"""
+    output = template.format(fontname, url)
+    of = open(outputfile_name, "w")
+    of.write(output)
