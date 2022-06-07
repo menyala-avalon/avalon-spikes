dec07385c78aad98f2de239710df442e11def097
Joel Lim <joel.lim@Joels-MacBook-Pro.local>
2022-06-07 17:24:08 +0800

    fetch more guild info

--- discord-playground/discordjsbot.ts
+++ discord-playground/discordjsbot.ts
@@ -13,20 +13,21 @@ bot.on('messageCreate', async (msg) => {
     console.log(`bot received ${content}`)
 
     async function analyseSentiment(content) {
-        if (!msg.author.bot) {
-            const { score } = await analyseSentiment(content)
-            msg.channel.send(`I heard you "${content} ${score} ${score > 0 ? "POSITIVE" : "NEGATIVE"}`)
-        }
+        const { score } = await analyseSentiment(content)
+        msg.channel.send(`I heard you "${content} ${score} ${score > 0 ? "POSITIVE" : "NEGATIVE"}`)
     }
 
-    // analyseSentiment(content)
+    if (!msg.author.bot) {
+        // await analyseSentiment(content)
+    }
 })
 
-bot.on('presenceUpdate', (oldPresence, newPresence) => {
+bot.on('presenceUpdate', async (oldPresence, newPresence) => {
     const { status, clientStatus, userId } = newPresence;
+    const channels = await newPresence.guild.channels.fetch()
+    const channel = channels[0]
     const msg = `user${userId} new status: ${status}, clientStatus: ${JSON.stringify(clientStatus, null, 2)}}`;
-    console.log(msg);
-    // channel.send(msg);
+    channel.send(msg);
 });
 
 bot.on('interactionCreate', async (interaction) => {
@@ -37,11 +38,25 @@ bot.on('ready', () => {
     console.log("bot is ready")
 })
 
-async function fetchGuildsInformation() {
-    const guildsResponse = await bot.guilds.fetch()
-    guildsResponse.each(async (guild) => {
-        const guildResponse = await guild.fetch()
-        console.log(`guild: ${guildResponse}`)
+async function fetchGuildsInformation(bot) {
+    const guilds = await bot.guilds.fetch()
+
+    guilds.each(async (guild) => {
+        const fetchedGuild = await guild.fetch()
+        const channelsManager = fetchedGuild.channels
+        const channels = await channelsManager.fetch()
+        const channelsOutput = (await Promise.all(channels.map(async (channel) => {
+            return `channel: ${channel.name}
+${channel.members.map(member => {
+                return member.displayName
+            }).join("\n")}
+`
+        }))).join("\n")
+
+        const output = `guild: ${fetchedGuild.name}
+${channelsOutput}
+`
+        console.log(output)
     })
 }
 
@@ -49,7 +64,8 @@ async function main() {
     console.log("connecting bot")
     await bot.login(process.env.DISCORD_TOKEN)
 
-    setInterval(fetchGuildsInformation, 10000)
+    await fetchGuildsInformation(bot)
+    // setInterval(fetchGuildsInformation, 10000)
 }
 
 main()
